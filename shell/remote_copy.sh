2b7ef39d177e131adf627f8fe231694882001fb3
Rick Thiessen <rickxix@ymail.com>
2018-01-12 10:16:55 -0600

    move remote copy pw to env var

--- shell/remote_copy.sh
+++ shell/remote_copy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 set ID [lindex $argv 0]
 set IP [lindex $argv 1]
-set PW [lindex $argv 2]
+set tempTitle [lindex $argv 2]
 set SD [lindex $argv 3]
 set timeout 60
 
@@ -9,7 +9,7 @@ spawn -noecho ksh -c "scp -o StrictHostKeyChecking=no $ID@$IP:~/transfer_temp/*$
 
 log_user 0
 expect "eDir Password:"
-send "$PW\r"
+send "$env($tempTitle)\r"
 log_user 1
 
 expect eof
