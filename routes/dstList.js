31cb3f74ee2d914e1cf60140eb477ca0221aae70
Thiessen, Rick - PHX <richard.thiessen@dignityhealth.org>
2018-11-15 14:24:57 -0700

    mutli-file check for adapter list api set

--- routes/dstList.js
+++ routes/dstList.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const glob = require('glob');
 const fs = require('fs');
 
 /* GET List of Destination Adapters */
 router.get('/', function(req, res, next) {
-  fs.readFile('./store/destAdapterList.csv', 'utf8', (err, data) => {
-    res.send(data);
-  });
+  var dstList = '';
+	
+	glob('./store/*.dst.*', (err, files) => {
+		files.forEach((file) => {
+			dstList += fs.readFileSync(file, 'utf8');
+		});
+		res.send(dstList);
+	});
 });
 
 module.exports = router;
--- routes/srcList.js
+++ routes/srcList.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const glob = require('glob');
 const fs = require('fs');
 
 /* GET Source Adapters */
 router.get('/', function(req, res, next) {
-  fs.readFile('./store/sourceAdapterList.csv', 'utf8', (err, data) => {
-    res.send(data);
-  });
+	var srcList = '';
+	
+	glob('./store/*.src.*', (err, files) => {
+		files.forEach((file) => {
+			srcList += fs.readFileSync(file, 'utf8');
+		});
+		res.send(srcList);
+	});
 });
 
 module.exports = router;
