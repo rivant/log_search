896864111f0ad6dae83f5f15a8cecdb45134838e
Thiessen, Rick - PHX <richard.thiessen@dignityhealth.org>
2019-08-12 12:26:29 -0700

    Move javascript to seperate file

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -1,10 +1,11 @@
 (function() {
-content = {  
+content = {
   srcMsgCount: 0,
   dstMsgCount: 0,
+  ackIndicator: 'color:red',
 	   
   toHtml: function(dataStr, searchedPattern){
-    let html = '<ul>'
+    let html = '';
     let delims = dataStr.split('DELIMITER');
     let search = new RegExp(this.escapeRegExp(searchedPattern), 'g');
     let srcStack = [];
@@ -20,19 +21,35 @@ content = {
     return html + '</ul>';
    },
 
-  srcToHtml: function(val, html){		
+  srcToHtml: function(val, html){
+    let response = /.*(RESPONSE SENT.*)/;
+    let ack = /.*(MSA\|.*)/;
+    let flattened = val.toString();
+
     if (val.length > 2){
       if (val[0] === '' || val[0] === '\r'){ val.shift(); }
-      
-      html += '<li>'+ val[0] +'</li>';  //correlation Id
-      html += '<ul><li style="color:blue">'+ val[1] +'</li><li style="color:blue">'+ val[5] +'</li><ul>';  //metadata				 
-      html += '<li>'+ val[2].replace(/[\v\x1C]/g, '').trim() +'</li>';  //message
+
+      html += '<ul><li>'+ val[0] +'</li>';  //correlation Id
+      html += '<ul><li style="color:blue">'+ val[1] +'</li><li style="color:blue">'+ val[5].split('  -')[1] +'</li>';  //metadata
+      html += '<ul><li>'+ val[2].replace(/[\v\x1C]/g, '').trim() +'</li></ul>';  //message
+
+      if (ack.test(flattened)) {
+        if (flattened.includes('MSA|AA')) {
+          this.ackIndicator = 'color:blue';
+        }
+        html += '<li style="'+ this.ackIndicator +'">'+ ack.exec(flattened) +'</li>';
+      }
+      if (response.test(flattened)) {
+        html += '<li style="'+ this.ackIndicator +'">'+ response.exec(flattened)[1] +'</li>';
+      }
+
       this.srcMsgCount++
-	
+
       if (val.toString().includes('DEST.log')){
-        html = this.destToHtml(val.toString(), html);				
+        html += '<hr>';
+        html = this.destToHtml(val.toString(), html);
       } else {
-        html += '</ul></ul><br/>';			
+        html += '</ul></ul><br/>';
       }
     }
     return html;
@@ -45,7 +62,6 @@ content = {
     let timeStamp = /(MESSAGE PROCESSING[\w\s:-]*),/;
     let msa = /INFO[\s\S]*(MSA\|A.*)/;
     let ack = /INFO[\s\S]*(POSITIVE.*|NACK A.*)/;
-    let indicator = 'color:red';
     let fileNameDst = [];
 
     if (fileArr){
@@ -56,15 +72,17 @@ content = {
           if (timeStamp.test(msgArr[idx + 1])) {
             html += '<li style="color:blue">'+ timeStamp.exec(msgArr[idx + 1])[1] +'</li><ul>';
           }
-          
+
           html += '<li>'+ msg.exec(msgArr[idx + 1])[1] +'</li></ul>';
-          
+
           if (msa.test(msgArr[idx + 1]) && ack.test(msgArr[idx + 1])) {
             if (msa.exec(msgArr[idx + 1])[1].includes('MSA|AA')) {
-              indicator = 'color:blue';
+              this.ackIndicator = 'color:blue';
+            } else {
+              this.ackIndicator = 'color:red';
             }
-            html += '<li style="'+ indicator +'">'+ msa.exec(msgArr[idx + 1])[1] +'</li>';
-            html += '<li style="'+ indicator +'">'+ ack.exec(msgArr[idx + 1])[1] +'</li>';
+            html += '<li style="'+ this.ackIndicator +'">'+ msa.exec(msgArr[idx + 1])[1] +'</li>';
+            html += '<li style="'+ this.ackIndicator +'">'+ ack.exec(msgArr[idx + 1])[1] +'</li>';
           }
 
           html += '</ul><br>';
