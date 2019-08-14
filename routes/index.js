c387e1e5468c4f796110af2354cf5bf8c8a1ee31
Thiessen, Rick - PHX <richard.thiessen@dignityhealth.org>
2019-08-14 13:31:59 -0700

    Move server info to config file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const readline = require('readline');
+const fs = require('fs');
+const readLocations = readline.createInterface({
+    input: fs.createReadStream('./store/location_config.json'),
+    output: process.stdout,
+    terminal: true
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {comm: res.connection.address()})
-})
+  res.render('index', {comm: res.connection.address(), sites: readLocations.history});
+});
 
 module.exports = router;
